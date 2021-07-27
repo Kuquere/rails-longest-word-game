afb6e90dbd1d6b869fde3d51fa5c10ef8e917b2a
carolina tordecilla <c.tordecilla.diaz@gmail.com>
2021-07-27 18:50:23 -0400

    en proceso

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,11 @@
 class GamesController < ApplicationController
   def new
+    vowels = ["a", "e", "i", "o", "u"]
+    @letters = Array.new(5) { vowels.sample }
+    @letters += Array.new(5) { ("a"..."z").to_a.sample }
   end
-  
+
   def score
+    @word = params[:word]
   end  
 end
