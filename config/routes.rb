ec4e3cb192162d59287917ff50ffc3534b7d0b56
carolina tordecilla <c.tordecilla.diaz@gmail.com>
2021-07-28 10:16:54 -0400

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  post '/score', to: 'games#score'
-  get '/new', to: 'games#new' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
